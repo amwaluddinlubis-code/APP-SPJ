7eae0d6204cb74df6fe63566cd8076f070eb934c
amwaluddinlubis-code <amwaluddin.lubis@gmail.com>
2026-09-06 19:58:15 +0700

    fix(spj): load package manual category controller

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,6 +3,7 @@ import './table-server-markers';
 import './table-ui-standardization';
 import './spj-purchase-date-validation';
 import './spj-package-document-placement';
+import './spj-package-manual-category';
 
 window.axios = axios;
 
