1744ac720d2c7ee6adad0d357bcc7310bf79d61f
amwaluddinlubis-code <amwaluddin.lubis@gmail.com>
2026-09-06 18:16:07 +0700

    style(spj): load package document placement

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import './table-server-markers';
 import './table-ui-standardization';
 import './spj-purchase-date-validation';
+import './spj-package-document-placement';
 
 window.axios = axios;
 
