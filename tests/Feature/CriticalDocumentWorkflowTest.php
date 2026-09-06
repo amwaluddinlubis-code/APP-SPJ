3497dc47349cfee8296288da57a365d68c2e03ef
amwaluddinlubis-code <amwaluddin.lubis@gmail.com>
2026-09-06 19:30:22 +0700

    test(spj): align invoice validation label with current checklist

--- tests/Feature/CriticalDocumentWorkflowTest.php
+++ tests/Feature/CriticalDocumentWorkflowTest.php
@@ -53,7 +53,7 @@ class CriticalDocumentWorkflowTest extends TestCase
         $this->assertCount(1, $receipt->items);
 
         $this->expectException(\RuntimeException::class);
-        $service->addGoodsReceipt($transaction, ['receipt_date' => '2026-01-21'], [['transaction_item_id' => $item->id, 'quantity_received' => 5, 'amount_received' => 500]]);
+        $service->addGoodsReceipt($transaction, ['payment_date' => '2026-02-02', 'gross_amount' => 500, 'tax_amount' => 0]);
     }
 
     public function test_entry_after_quarter_numbering_is_marked_late(): void
@@ -213,7 +213,7 @@ class CriticalDocumentWorkflowTest extends TestCase
 
         $issues = app(SpjPackageValidationService::class)->validate($package->load(['transaction.items', 'transaction.goods']));
 
-        $this->assertContains('Duplikasi invoice', collect($issues)->pluck('label')->all());
+        $this->assertContains('Keunikan invoice', collect($issues)->pluck('label')->all());
     }
 
     private function readyGoodsTransaction(): Transaction
