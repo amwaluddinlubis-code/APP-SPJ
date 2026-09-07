224d84ef3d41dcb01277861cc0f03775d0b2450b
amwaluddinlubis-code <amwaluddin.lubis@gmail.com>
2026-09-08 06:55:24 +0700

    fix: remove Vite asset loading from toast component

--- resources/views/components/toast-notifications.blade.php
+++ resources/views/components/toast-notifications.blade.php
@@ -1,10 +1,3 @@
-@vite('resources/js/theme-profiles.js')
-@vite('resources/js/maintenance-transaction-links.js')
-@vite('resources/js/transaction-category-controls.js')
-@vite('resources/js/spj-preparation-state-normalizer.js')
-@vite('resources/js/transaction-summary-three-column.js')
-@vite('resources/js/legacy-action-icon-migrator.js')
-
 <x-ui.icon-templates />
 
 @php
