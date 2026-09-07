512b2f125751ae0d69add44f306bf71a413bd5bc
amwaluddinlubis-code <amwaluddin.lubis@gmail.com>
2026-09-08 02:08:07 +0700

    Render SPJ report tabs server-side reliably

--- resources/views/spj/partials/main-tabs.blade.php
+++ resources/views/spj/partials/main-tabs.blade.php
@@ -1,21 +1,20 @@
+@php($activeSpjTab = $tab ?? 'persiapan')
+
 <div id="spj-main-tabs">
     <x-tabs :tabs="[
         ['id' => 'persiapan', 'label' => '📦 Persiapan'],
         ['id' => 'paket', 'label' => '📄 Paket'],
         ['id' => 'laporan', 'label' => '📊 Laporan'],
         ['id' => 'monitoring', 'label' => '⚠️ Monitoring'],
-    ]" :activeTab="$tab" />
+    ]" :activeTab="$activeSpjTab" />
 </div>
 
-{{-- Laporan dan Monitoring dirender berdasarkan tab server-side.
-     Navigasi tab melakukan full URL navigation. x-ignore mencegah x-show legacy
-     di dalam partial memproses ulang visibilitas panel aktif. --}}
-@if(($tab ?? 'persiapan') === 'laporan')
-    <div x-ignore data-spj-server-tab="laporan">
+@switch($activeSpjTab)
+    @case('laporan')
         @include('spj.partials.laporan')
-    </div>
-@elseif(($tab ?? 'persiapan') === 'monitoring')
-    <div x-ignore data-spj-server-tab="monitoring">
+        @break
+
+    @case('monitoring')
         @include('spj.partials.monitoring')
-    </div>
-@endif
+        @break
+@endswitch
