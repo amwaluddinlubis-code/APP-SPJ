89bb18fb66d47b1587f0eb22d94b707e35f2c3ab
amwaluddinlubis-code <amwaluddin.lubis@gmail.com>
2026-09-07 08:48:55 +0700

    fix(config): make SPJ data root portable by default

--- config/spj.php
+++ config/spj.php
@@ -5,7 +5,7 @@ return [
     'school_mode' => env('SPJ_SCHOOL_MODE', 'single-school'),
     'fiscal_year_mode' => env('SPJ_FISCAL_YEAR_MODE', 'single-year'),
     'backup_retention' => (int) env('SPJ_BACKUP_RETENTION', 30),
-    'data_path' => env('SPJ_DATA_PATH', 'D:/lrvProject/spj-bosp-data'),
+    'data_path' => env('SPJ_DATA_PATH', storage_path('app')),
     'database_manager_sensitive_columns' => [
         'password',
         'token',
