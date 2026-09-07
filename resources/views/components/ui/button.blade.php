a1f86075d33848282c382bc26068458f5921579d
amwaluddinlubis-code <amwaluddin.lubis@gmail.com>
2026-09-07 22:06:00 +0700

    refactor(ui): add canonical icon support to buttons

--- resources/views/components/ui/button.blade.php
+++ resources/views/components/ui/button.blade.php
@@ -2,12 +2,34 @@
     'variant' => 'primary',
     'type' => 'button',
     'href' => null,
+    'icon' => null,
+    'iconPosition' => 'start',
+    'iconSize' => 'sm',
 ])
 
-@php($classes = 'ui-btn ui-btn-'.$variant)
+@php
+    $classes = 'ui-btn ui-btn-'.$variant;
+    $iconPosition = in_array($iconPosition, ['start', 'end'], true) ? $iconPosition : 'start';
+@endphp
 
 @if($href)
-    <a href="{{ $href }}" {{ $attributes->class([$classes]) }}>{{ $slot }}</a>
+    <a href="{{ $href }}" {{ $attributes->class([$classes]) }}>
+        @if($icon && $iconPosition === 'start')
+            <x-ui.icon :name="$icon" :size="$iconSize" />
+        @endif
+        {{ $slot }}
+        @if($icon && $iconPosition === 'end')
+            <x-ui.icon :name="$icon" :size="$iconSize" />
+        @endif
+    </a>
 @else
-    <button type="{{ $type }}" {{ $attributes->class([$classes]) }}>{{ $slot }}</button>
+    <button type="{{ $type }}" {{ $attributes->class([$classes]) }}>
+        @if($icon && $iconPosition === 'start')
+            <x-ui.icon :name="$icon" :size="$iconSize" />
+        @endif
+        {{ $slot }}
+        @if($icon && $iconPosition === 'end')
+            <x-ui.icon :name="$icon" :size="$iconSize" />
+        @endif
+    </button>
 @endif
