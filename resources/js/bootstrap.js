8a9317b5471742771db383649ee122003ea7bdab
amwaluddinlubis-code <amwaluddin.lubis@gmail.com>
2026-09-08 04:27:28 +0700

    feat: load SPJ package transaction boundary UI

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,6 +4,7 @@ import './table-ui-standardization';
 import './spj-purchase-date-validation';
 import './spj-package-document-placement';
 import './spj-package-manual-category';
+import './spj-package-transaction-boundary';
 import './transaction-detail-ui';
 import './transaction-detail-common-fields-layout';
 import './transaction-detail-category-layout';
