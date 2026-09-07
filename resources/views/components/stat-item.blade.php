6e57018c50a9e2f6069728a9c4c4186d75416588
amwaluddinlubis-code <amwaluddin.lubis@gmail.com>
2026-09-08 00:00:26 +0700

    feat(ui): support icons in summary stat items

--- resources/views/components/stat-item.blade.php
+++ resources/views/components/stat-item.blade.php
@@ -3,10 +3,21 @@
     'value',
     'hint' => null,
     'valueClass' => null,
+    'icon' => null,
+    'iconClass' => null,
 ])
 
 <div {{ $attributes->class(['ui-stat']) }}>
-    <p class="ui-stat-label">{{ $label }}</p>
+    @if($icon)
+        <div class="ui-stat-heading">
+            <span class="ui-stat-icon {{ $iconClass }}">
+                <x-ui-icon :name="$icon" class="h-5 w-5" />
+            </span>
+            <p class="ui-stat-label">{{ $label }}</p>
+        </div>
+    @else
+        <p class="ui-stat-label">{{ $label }}</p>
+    @endif
     <p class="ui-stat-value {{ $valueClass }}">{{ $value }}</p>
     @if($hint)
         <p class="ui-stat-hint">{{ $hint }}</p>
