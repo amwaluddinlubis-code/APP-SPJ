2fda1c5b69f5c4881de743be3cf16ed52d536505
amwaluddinlubis-code <amwaluddin.lubis@gmail.com>
2026-09-08 06:55:11 +0700

    refactor: load global UI modules from app bundle

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import './theme-profiles';
 import './table-server-markers';
 import './table-ui-standardization';
 import './spj-purchase-date-validation';
@@ -6,6 +7,7 @@ import './spj-package-document-placement';
 import './spj-package-manual-category';
 import './spj-package-maintenance-links';
 import './spj-package-transaction-boundary';
+import './legacy-action-icon-migrator';
 import './action-icon-deduplicator';
 
 window.axios = axios;
