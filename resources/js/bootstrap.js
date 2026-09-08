04a8763a587130fb0f9c3ffd34d8567afa982eb6
amwaluddinlubis-code <amwaluddin.lubis@gmail.com>
2026-09-08 07:03:00 +0700

    fix: load transaction action modal

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import './theme-profiles';
 import './table-server-markers';
 import './table-ui-standardization';
+import './transaction-action-modal';
 import './spj-purchase-date-validation';
 import './spj-package-document-placement';
 import './spj-package-manual-category';
