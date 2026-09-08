adb0d3eb9a1f9e0e1b4f75c76a70d9a41a647a15
amwaluddinlubis-code <amwaluddin.lubis@gmail.com>
2026-09-08 21:00:46 +0700

    style: remove unused dashboard import

--- app/Http/Controllers/ProductivityDashboardController.php
+++ app/Http/Controllers/ProductivityDashboardController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Transaction;
 use App\Services\SpjWorkflowFilterService;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\View\View;
 
 class ProductivityDashboardController extends Controller
