15e4b8f5ac14cbadd25faa309856c9ed8e5bf22c
amwaluddinlubis-code <amwaluddin.lubis@gmail.com>
2026-09-10 02:41:02 +0700

    feat: expose ARKAS import delta metrics

--- app/Models/ArkasImportRun.php
+++ app/Models/ArkasImportRun.php
@@ -9,7 +9,20 @@ class ArkasImportRun extends Model
 {
     protected $connection = 'school';
 
-    protected $fillable = ['profile_id', 'fiscal_year_id', 'status', 'records_read', 'records_written', 'message', 'started_at', 'finished_at'];
+    protected $fillable = [
+        'profile_id',
+        'fiscal_year_id',
+        'status',
+        'records_read',
+        'records_written',
+        'records_new',
+        'records_changed',
+        'records_unchanged',
+        'records_removed',
+        'message',
+        'started_at',
+        'finished_at',
+    ];
 
     protected function casts(): array
     {
