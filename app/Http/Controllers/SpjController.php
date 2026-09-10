a6c4fcce6e00f627e23fc89135ee69682faf819d
amwaluddinlubis-code <amwaluddin.lubis@gmail.com>
2026-09-10 14:06:34 +0700

    refactor: route SPJ actions to focused use cases

--- app/Http/Controllers/SpjController.php
+++ app/Http/Controllers/SpjController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\UseCases\Spj\SpjDocumentLifecycleUseCase;
 use App\UseCases\Spj\SpjDocumentUseCase;
-use App\UseCases\Spj\SpjNumberingUseCase;
+use App\UseCases\Spj\SpjFiscalPeriodUseCase;
 use App\UseCases\Spj\SpjPackageCategoryUseCase;
+use App\UseCases\Spj\SpjPackageLifecycleUseCase;
+use App\UseCases\Spj\SpjQuarterNumberingUseCase;
 use App\UseCases\Spj\SpjReportUseCase;
+use App\UseCases\Spj\SpjSettlementUseCase;
+use App\UseCases\Spj\SpjSingleNumberingUseCase;
 use App\UseCases\Spj\SpjWorkspaceUseCase;
 use App\UseCases\Spj\UpdateSpjPackageDetailsUseCase;
 use Illuminate\Http\JsonResponse;
@@ -20,62 +25,62 @@ class SpjController extends Controller
         return $useCase->handle($request);
     }
 
-    public function assignNumber(string $packageId, SpjNumberingUseCase $useCase): RedirectResponse
+    public function assignNumber(string $packageId, SpjSingleNumberingUseCase $useCase): RedirectResponse
     {
         return $useCase->assignNumber($packageId);
     }
 
-    public function markReady(string $packageId, SpjNumberingUseCase $useCase): RedirectResponse
+    public function markReady(string $packageId, SpjPackageLifecycleUseCase $useCase): RedirectResponse
     {
         return $useCase->markReady($packageId);
     }
 
-    public function assignQuarterNumbers(Request $request, SpjNumberingUseCase $useCase): RedirectResponse
+    public function assignQuarterNumbers(Request $request, SpjQuarterNumberingUseCase $useCase): RedirectResponse
     {
         return $useCase->assignQuarterNumbers($request);
     }
 
-    public function assignDocumentNumber(Request $request, string $packageId, string $documentType, SpjNumberingUseCase $useCase): RedirectResponse
+    public function assignDocumentNumber(Request $request, string $packageId, string $documentType, SpjSingleNumberingUseCase $useCase): RedirectResponse
     {
         return $useCase->assignDocumentNumber($request, $packageId, $documentType);
     }
 
-    public function finalizeDocument(string $documentId, SpjNumberingUseCase $useCase): RedirectResponse
+    public function finalizeDocument(string $documentId, SpjDocumentLifecycleUseCase $useCase): RedirectResponse
     {
         return $useCase->finalizeDocument($documentId);
     }
 
-    public function cancelDocument(Request $request, string $documentId, SpjNumberingUseCase $useCase): RedirectResponse
+    public function cancelDocument(Request $request, string $documentId, SpjDocumentLifecycleUseCase $useCase): RedirectResponse
     {
         return $useCase->cancelDocument($request, $documentId);
     }
 
-    public function replaceDocument(Request $request, string $documentId, SpjNumberingUseCase $useCase): RedirectResponse
+    public function replaceDocument(Request $request, string $documentId, SpjDocumentLifecycleUseCase $useCase): RedirectResponse
     {
         return $useCase->replaceDocument($request, $documentId);
     }
 
-    public function closeQuarter(Request $request, SpjNumberingUseCase $useCase): RedirectResponse
+    public function closeQuarter(Request $request, SpjFiscalPeriodUseCase $useCase): RedirectResponse
     {
         return $useCase->closeQuarter($request);
     }
 
-    public function reopenQuarter(Request $request, string $periodId, SpjNumberingUseCase $useCase): RedirectResponse
+    public function reopenQuarter(Request $request, string $periodId, SpjFiscalPeriodUseCase $useCase): RedirectResponse
     {
         return $useCase->reopenQuarter($request, $periodId);
     }
 
-    public function storePayment(Request $request, string $transactionId, SpjNumberingUseCase $useCase): RedirectResponse
+    public function storePayment(Request $request, string $transactionId, SpjSettlementUseCase $useCase): RedirectResponse
     {
         return $useCase->storePayment($request, $transactionId);
     }
 
-    public function storeGoodsReceipt(Request $request, string $transactionId, SpjNumberingUseCase $useCase): RedirectResponse
+    public function storeGoodsReceipt(Request $request, string $transactionId, SpjSettlementUseCase $useCase): RedirectResponse
     {
         return $useCase->storeGoodsReceipt($request, $transactionId);
     }
 
-    public function unlockPackage(Request $request, string $packageId, SpjNumberingUseCase $useCase): RedirectResponse
+    public function unlockPackage(Request $request, string $packageId, SpjPackageLifecycleUseCase $useCase): RedirectResponse
     {
         return $useCase->unlockPackage($request, $packageId);
     }
