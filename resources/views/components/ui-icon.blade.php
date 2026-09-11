2b24eb8f4e9328a82efa99b5c00e2a0d88a1015d
amwaluddinlubis-code <amwaluddin.lubis@gmail.com>
2026-09-11 22:02:01 +0700

    refactor(gui): make legacy icon wrapper delegate to canonical registry

--- resources/views/components/ui-icon.blade.php
+++ resources/views/components/ui-icon.blade.php
@@ -1,34 +1,5 @@
-@php($paths = [
-    'dashboard' => 'M3 13h8V3H3v10Zm0 8h8v-6H3v6Zm10 0h8V11h-8v10Zm0-18v6h8V3h-8Z',
-    'budget' => 'M4 19h16M6 17V9m4 8V5m4 12v-6m4 6V3',
-    'transaction' => 'm7 7 5-5 5 5m-5-5v16m5-4-5 5-5-5',
-    'tax' => 'M9 14h6m-7-4h8m-1-7H9a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h6l4-4V5a2 2 0 0 0-2-2Zm2 14v-4h4',
-    'document' => 'M6 2h8l4 4v16H6a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2Zm8 0v5h5M8 12h8M8 16h6',
-    'report' => 'M4 19V5m0 14h16M8 16v-5m4 5V7m4 9v-8',
-    'audit' => 'M4 5h16v14H4zM8 9h8M8 13h5M8 17h3',
-    'database' => 'M4 5c0-1.1 3.6-2 8-2s8 .9 8 2-3.6 2-8 2-8-.9-8-2Zm0 0v7c0 1.1 3.6 2 8 2s8-.9 8-2V5m-16 7v7c0 1.1 3.6 2 8 2s8-.9 8-2v-7',
-    'sync' => 'M20 11a8.1 8.1 0 0 0-14.8-4L3 10m0 0V4m0 6h6M4 13a8.1 8.1 0 0 0 14.8 4L21 14m0 0v6m0-6h-6',
-    'calendar' => 'M6 3v3m12-3v3M4 9h16M5 5h14a1 1 0 0 1 1 1v13H4V6a1 1 0 0 1 1-1Z',
-    'settings' => 'M12 15.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm0-12v2m0 13v2m8.5-8.5h-2m-13 0h-2m14.2-6.2-1.4 1.4M5.7 18.3l-1.4 1.4m14.4 0-1.4-1.4M5.7 5.7 4.3 4.3',
-    'archive' => 'M3 7h18M5 7l1-4h12l1 4M5 7v13h14V7M9 11h6',
-    'server' => 'M4 4h16v6H4zM4 14h16v6H4zM7 7h.01M7 17h.01',
-    'logout' => 'M10 17l5-5-5-5m5 5H3m9-9V3h9v18h-9v-2',
-    'edit' => 'M4 20h4l11-11a2.8 2.8 0 0 0-4-4L4 16v4Zm10-14 4 4M13 20h7',
-    'employee' => 'M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2M9 11a4 4 0 1 0 0-8 4 4 0 0 0 0 8Zm13 10v-2a4 4 0 0 1 0 7.75',
-    'search' => 'm21 21-4.35-4.35M19 11a8 8 0 1 1-16 0 8 8 0 0 1 16 0Z',
-    'preview' => 'M2.5 12s3.5-6 9.5-6 9.5 6 9.5 6-3.5 6-9.5 6-9.5-6-9.5-6Zm9.5 3a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z',
-    'excel' => 'M5 3h10l4 4v14H5V3Zm10 0v5h5M8 12h8m-8 4h8m-8-8h2',
-    'pdf' => 'M5 3h10l4 4v14H5V3Zm10 0v5h5M8 16h2.5a1.5 1.5 0 0 0 0-3H8v3Zm6 0v-3h1a1.5 1.5 0 0 1 0 3h-1Z',
-    'inbox' => 'M4 5h16v14H4V5Zm0 9h4l2 3h4l2-3h4',
-    'work' => 'M9 6V4h6v2m-10 0h14a2 2 0 0 1 2 2v10H3V8a2 2 0 0 1 2-2Zm-2 5h18M9 11v2h6v-2',
-    'number' => 'M8 3 6 21m10-18-2 18M4 9h16M3 15h16',
-    'clock' => 'M12 22a10 10 0 1 0 0-20 10 10 0 0 0 0 20Zm0-15v5l3 2',
-    'priority' => 'M12 3 4 7v5c0 5 3.4 8.2 8 9 4.6-.8 8-4 8-9V7l-8-4Zm0 5v5m0 4h.01',
-    'progress' => 'M4 19V5m0 14h16M8 15v-4m4 4V7m4 8v-6',
-    'queue' => 'M5 6h14M5 12h14M5 18h9',
-    'system' => 'M12 3v3m0 12v3m9-9h-3M6 12H3m15.4-6.4-2.1 2.1M7.7 16.3l-2.1 2.1m12.8 0-2.1-2.1M7.7 7.7 5.6 5.6M12 16a4 4 0 1 0 0-8 4 4 0 0 0 0 8Z',
-    'balance' => 'M3 7h18v12H3V7Zm0 0 3-4h12l3 4M16 12h5v4h-5a2 2 0 1 1 0-4Z',
-    'chevron-left' => 'm15 18-6-6 6-6',
-    'chevron-right' => 'm9 18 6-6-6-6',
-])
-<svg {{ $attributes->merge(['class' => 'h-5 w-5 shrink-0', 'fill' => 'none', 'viewBox' => '0 0 24 24', 'stroke' => 'currentColor', 'stroke-width' => '1.8']) }} aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" d="{{ $paths[$name] ?? $paths['document'] }}" /></svg>
+@props(['name'])
+
+{{-- Compatibility wrapper for legacy <x-ui-icon> consumers.
+     New or touched markup must use <x-ui.icon> directly. --}}
+<x-ui.icon :name="$name" {{ $attributes }} />
