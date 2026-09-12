861a966711c582e99bd24026fe7f74590d142b61
amwaluddinlubis-code <amwaluddin.lubis@gmail.com>
2026-09-12 09:22:20 +0700

    test: align quarter token expectations without TW prefix

--- tests/Unit/QuarterNumberingPlaceholderTest.php
+++ tests/Unit/QuarterNumberingPlaceholderTest.php
@@ -19,12 +19,12 @@ class QuarterNumberingPlaceholderTest extends TestCase
         ]);
         $service = $this->numberingService();
 
-        $this->assertSame('0001/OP/SDN.318/TW.I/2026', $service->renderConfiguredNumber($format, 'PESANAN', 1, Carbon::parse('2026-01-08'), 'SDN.318'));
-        $this->assertSame('0002/OP/SDN.318/TW.I/2026', $service->renderConfiguredNumber($format, 'PESANAN', 2, Carbon::parse('2026-02-03'), 'SDN.318'));
-        $this->assertSame('0003/OP/SDN.318/TW.I/2026', $service->renderConfiguredNumber($format, 'PESANAN', 3, Carbon::parse('2026-03-31'), 'SDN.318'));
-        $this->assertSame('0004/OP/SDN.318/TW.II/2026', $service->renderConfiguredNumber($format, 'PESANAN', 4, Carbon::parse('2026-04-01'), 'SDN.318'));
-        $this->assertSame('0005/OP/SDN.318/TW.III/2026', $service->renderConfiguredNumber($format, 'PESANAN', 5, Carbon::parse('2026-07-01'), 'SDN.318'));
-        $this->assertSame('0006/OP/SDN.318/TW.IV/2026', $service->renderConfiguredNumber($format, 'PESANAN', 6, Carbon::parse('2026-10-01'), 'SDN.318'));
+        $this->assertSame('0001/OP/SDN.318/I/2026', $service->renderConfiguredNumber($format, 'PESANAN', 1, Carbon::parse('2026-01-08'), 'SDN.318'));
+        $this->assertSame('0002/OP/SDN.318/I/2026', $service->renderConfiguredNumber($format, 'PESANAN', 2, Carbon::parse('2026-02-03'), 'SDN.318'));
+        $this->assertSame('0003/OP/SDN.318/I/2026', $service->renderConfiguredNumber($format, 'PESANAN', 3, Carbon::parse('2026-03-31'), 'SDN.318'));
+        $this->assertSame('0004/OP/SDN.318/II/2026', $service->renderConfiguredNumber($format, 'PESANAN', 4, Carbon::parse('2026-04-01'), 'SDN.318'));
+        $this->assertSame('0005/OP/SDN.318/III/2026', $service->renderConfiguredNumber($format, 'PESANAN', 5, Carbon::parse('2026-07-01'), 'SDN.318'));
+        $this->assertSame('0006/OP/SDN.318/IV/2026', $service->renderConfiguredNumber($format, 'PESANAN', 6, Carbon::parse('2026-10-01'), 'SDN.318'));
     }
 
     public function test_legacy_roman_month_placeholder_remains_backward_compatible(): void
