d0c1468a6b8297ae9c6791e0fdb0b2951ccd142c
amwaluddinlubis-code <amwaluddin.lubis@gmail.com>
2026-09-12 10:34:35 +0700

    ui: track payment and item description edits together

--- resources/views/transactions/show.blade.php
+++ resources/views/transactions/show.blade.php
@@ -16,7 +16,7 @@
         $needsAttention = $sourceStatus === 'SOURCE_MISSING' || (bool) $transaction->requires_reconciliation;
     @endphp
 
-    <div class="flex flex-col gap-6" x-data="{ itemDescriptionsDirty: false }">
+    <div class="flex flex-col gap-6" x-data="{ spjDescriptionsDirty: false }">
         @include('transactions.partials.detail.overview-header')
         @include('transactions.partials.detail.overview-status')
         @include('transactions.partials.detail.source-reconciliation')
