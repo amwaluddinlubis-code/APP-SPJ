35005a82f8b6dbd3276d64f73a50818e878680db
amwaluddinlubis-code <amwaluddin.lubis@gmail.com>
2026-09-15 03:35:46 +0700

    Separate periodic reports from SPJ reports in sidebar

--- resources/views/components/layouts/partials/spj-report-navigation.blade.php
+++ resources/views/components/layouts/partials/spj-report-navigation.blade.php
@@ -1,5 +1,6 @@
 @php
     $isSpjReportRoute = request()->routeIs('spj.*') && request('tab') === 'laporan';
+    $isPeriodicReportRoute = $isSpjReportRoute && request('jenis_laporan') === 'periode';
     $activeReportScope = (string) request('paket_laporan', 'bulan');
     $reportScopes = [
         ['key' => 'bulan', 'label' => 'Bulanan', 'icon' => 'calendar'],
@@ -9,25 +10,35 @@
     ];
 @endphp
 
-<div x-data="{ reportMenuOpen: {{ $isSpjReportRoute ? 'true' : 'false' }} }">
+<a class="app-nav {{ $isSpjReportRoute && ! $isPeriodicReportRoute ? 'app-nav-active' : '' }}"
+    href="{{ route('spj.index', ['tab' => 'laporan']) }}" title="Laporan SPJ">
+    <x-ui.icon name="report" />
+    <span class="nav-label">Laporan SPJ</span>
+</a>
+
+<div x-data="{ reportMenuOpen: {{ $isPeriodicReportRoute ? 'true' : 'false' }} }">
     <button type="button"
         @click="reportMenuOpen = !reportMenuOpen"
         :aria-expanded="reportMenuOpen.toString()"
-        aria-controls="nav-spj-reports"
-        class="app-nav w-full text-left {{ $isSpjReportRoute ? 'app-nav-section-active' : '' }}"
-        title="Laporan SPJ">
-        <x-ui.icon name="report" />
-        <span class="nav-label flex-1">Laporan SPJ</span>
+        aria-controls="nav-periodic-reports"
+        class="app-nav w-full text-left {{ $isPeriodicReportRoute ? 'app-nav-section-active' : '' }}"
+        title="Laporan Periode">
+        <x-ui.icon name="calendar" />
+        <span class="nav-label flex-1">Laporan Periode</span>
         <x-ui.icon name="chevron-down" size="xs" class="transition-transform"
             ::class="reportMenuOpen ? 'rotate-180' : ''" />
     </button>
 
-    <div id="nav-spj-reports" x-show="reportMenuOpen" x-collapse
+    <div id="nav-periodic-reports" x-show="reportMenuOpen" x-collapse
         class="app-nav-submenu ml-5 space-y-1 border-l pl-2">
         @foreach ($reportScopes as $reportScope)
-            <a class="app-nav {{ $isSpjReportRoute && $activeReportScope === $reportScope['key'] ? 'app-nav-active' : '' }}"
-                href="{{ route('spj.index', ['tab' => 'laporan', 'paket_laporan' => $reportScope['key']]) }}"
-                title="Laporan {{ $reportScope['label'] }}">
+            <a class="app-nav {{ $isPeriodicReportRoute && $activeReportScope === $reportScope['key'] ? 'app-nav-active' : '' }}"
+                href="{{ route('spj.index', [
+                    'tab' => 'laporan',
+                    'jenis_laporan' => 'periode',
+                    'paket_laporan' => $reportScope['key'],
+                ]) }}"
+                title="Laporan Periode {{ $reportScope['label'] }}">
                 <x-ui.icon :name="$reportScope['icon']" size="xs" />
                 <span class="nav-label">{{ $reportScope['label'] }}</span>
             </a>
