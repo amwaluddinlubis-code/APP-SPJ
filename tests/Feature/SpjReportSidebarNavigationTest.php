540e5b528cbbefa24b9127daaae15be5891b1673
amwaluddinlubis-code <amwaluddin.lubis@gmail.com>
2026-09-15 03:37:27 +0700

    Test separate periodic report navigation surface

--- tests/Feature/SpjReportSidebarNavigationTest.php
+++ tests/Feature/SpjReportSidebarNavigationTest.php
@@ -6,7 +6,7 @@ use Tests\TestCase;
 
 class SpjReportSidebarNavigationTest extends TestCase
 {
-    public function test_sidebar_uses_dedicated_periodic_report_navigation_partial(): void
+    public function test_sidebar_uses_dedicated_report_navigation_partial(): void
     {
         $layout = file_get_contents(resource_path('views/components/layouts/tailwind-app.blade.php'));
 
@@ -17,14 +17,16 @@ class SpjReportSidebarNavigationTest extends TestCase
         );
     }
 
-    public function test_report_navigation_exposes_parent_and_four_period_submenus(): void
+    public function test_spj_report_and_periodic_report_are_separate_sidebar_entries(): void
     {
         $partial = file_get_contents(resource_path('views/components/layouts/partials/spj-report-navigation.blade.php'));
 
         $this->assertIsString($partial);
-        $this->assertStringContainsString('Laporan SPJ', $partial);
-        $this->assertStringContainsString('aria-controls="nav-spj-reports"', $partial);
+        $this->assertStringContainsString('>Laporan SPJ</span>', $partial);
+        $this->assertStringContainsString('>Laporan Periode</span>', $partial);
+        $this->assertStringContainsString('aria-controls="nav-periodic-reports"', $partial);
         $this->assertStringContainsString('reportMenuOpen', $partial);
+        $this->assertStringContainsString("request('jenis_laporan') === 'periode'", $partial);
         $this->assertStringContainsString("request('paket_laporan', 'bulan')", $partial);
 
         foreach (['bulan', 'triwulan', 'semester', 'tahunan'] as $scope) {
@@ -36,6 +38,19 @@ class SpjReportSidebarNavigationTest extends TestCase
         }
 
         $this->assertStringContainsString("'tab' => 'laporan'", $partial);
+        $this->assertStringContainsString("'jenis_laporan' => 'periode'", $partial);
         $this->assertStringContainsString("'paket_laporan' => \$reportScope['key']", $partial);
+    }
+
+    public function test_periodic_report_uses_a_dedicated_surface_instead_of_spj_report_history(): void
+    {
+        $component = file_get_contents(app_path('Livewire/SpjReportFilter.php'));
+        $periodicView = file_get_contents(resource_path('views/livewire/spj-periodic-report-page.blade.php'));
+
+        $this->assertIsString($component);
+        $this->assertIsString($periodicView);
+        $this->assertStringContainsString("request('jenis_laporan') === 'periode'", $component);
+        $this->assertStringContainsString("view('livewire.spj-periodic-report-page')", $component);
+        $this->assertStringContainsString('<livewire:spj-periodic-report-center', $periodicView);
     }
 }
