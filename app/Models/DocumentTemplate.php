6a32b82058cca58cb1baa7a0e68e2ebb5ce1e2fe
amwaluddinlubis-code <amwaluddin.lubis@gmail.com>
2026-09-16 05:41:53 +0700

    feat: expose template SiPlah mapping

--- app/Models/DocumentTemplate.php
+++ app/Models/DocumentTemplate.php
@@ -9,7 +9,16 @@ class DocumentTemplate extends Model
 {
     protected $connection = 'school';
 
-    protected $fillable = ['fiscal_year_id', 'document_type', 'name', 'format', 'file_path', 'applicable_categories', 'is_active'];
+    protected $fillable = [
+        'fiscal_year_id',
+        'document_type',
+        'name',
+        'format',
+        'file_path',
+        'applicable_categories',
+        'is_siplah',
+        'is_active',
+    ];
 
     public function fiscalYear(): BelongsTo
     {
@@ -18,6 +27,10 @@ class DocumentTemplate extends Model
 
     protected function casts(): array
     {
-        return ['is_active' => 'boolean', 'applicable_categories' => 'array'];
+        return [
+            'is_active' => 'boolean',
+            'is_siplah' => 'boolean',
+            'applicable_categories' => 'array',
+        ];
     }
 }
