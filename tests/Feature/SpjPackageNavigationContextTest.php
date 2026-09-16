3773f4081641e4431a009e4e128e4a3382f08659
amwaluddinlubis-code <amwaluddin.lubis@gmail.com>
2026-09-16 10:56:25 +0700

    test: keep package detail list query eliminated

--- tests/Feature/SpjPackageNavigationContextTest.php
+++ tests/Feature/SpjPackageNavigationContextTest.php
@@ -7,7 +7,6 @@ use App\Models\FundSource;
 use App\Models\SpjPackage;
 use App\Models\Transaction;
 use App\UseCases\Spj\SpjWorkspaceUseCase;
-use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
@@ -60,7 +59,7 @@ class SpjPackageNavigationContextTest extends TestCase
         $response = app(SpjWorkspaceUseCase::class)->handle($request);
 
         $this->assertInstanceOf(View::class, $response);
-        $this->assertInstanceOf(LengthAwarePaginator::class, $response->getData()['packageList']);
+        $this->assertNull($response->getData()['packageList']);
         $this->assertSame($expectedPrevious->id, $response->getData()['previousPackageId']);
         $this->assertSame($expectedNext->id, $response->getData()['nextPackageId']);
     }
